19e0bc3de5cc022399dd61494e9218971db9a6a4
naveenpitchai <naveenk.89@hotmail.com>
2017-03-06 16:00:47 -0500

    adding the initial version before k-best,gridcv implementation

--- enron-poi-identifier/code-and-data/poi_id.py
+++ enron-poi-identifier/code-and-data/poi_id.py
@@ -26,14 +26,7 @@ from feature_format import featureFormat, targetFeatureSplit
 from email_process import email_parser
 from tester import dump_classifier_and_data, main
 from email_model import email_fit
-from feature_selection import  balance_data
 import tester
-from feature_selection import gridtest, feature_filter, kbest_scores
-
-
-def pause():
-    inp = raw_input("Press <ENTER> to continue...\n")
-
 
 ####################Project Instructions###########################################
 
@@ -68,7 +61,10 @@ zipped = False
 #this will also skip the final plot of the classifier with 2 features.
 least_component = True
 
+
 ####################################################################################
+
+
 
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
@@ -118,7 +114,8 @@ data_numpy = featureFormat(data_dict, features_list, sort_keys = True)
 scatter_plots(data_numpy[:,1],data_numpy[:,2],'plot after removing outlier')
 
 #Although the data set has some more extreme values in the dataset those are keys data points,
-#so decided to leave them in
+# so decided to leave them in
+
 
 ### Task 3: Create new feature(s)
 
@@ -128,11 +125,20 @@ print '################# Starting feature creation ####################\n'
 #Fraction of outgoing poi emails and incoming poi emails
 
 #calling the feature creation function to create new feature.
-my_dataset = new_feature(data_dict)
+data_dict = new_feature(data_dict)
+
+#putting the data in to my_dataset after adding the features
+my_dataset = data_dict
 
 print 'New fraction features created based on email interactions with POI.\n'
 
-pause()
+#Making two different feature sets to train
+
+features_list1 = ['poi','salary','total_payments','from_this_person_to_poi','from_poi_to_this_person',
+                 'shared_receipt_with_poi','bonus','total_stock_value','from_poi_frac','to_poi_frac']
+
+features_list2 = ['poi','salary','total_payments',
+                 'shared_receipt_with_poi','bonus', 'total_stock_value','from_poi_frac','to_poi_frac']
 
 
 ### Task 4: Try a varity of classifiers
@@ -143,63 +149,49 @@ pause()
 
 #Trying an SVM model with linear kernel and evaluating against the tester.py
 
-print '##################### INITIAL TESTS ON MULTIPLE MODELS ######################'
+print '##################### TESTS ON MULTIPLE MODELS ######################'
 
 print '\nFitting and testing multiple classifiers\n'
 
-all_features = ['poi','exercised_stock_options','total_stock_value','salary','bonus',
-               'shared_receipt_with_poi','expenses','from_poi_to_this_person','total_payments',
-               'loan_advances','other','deferred_income','from_this_person_to_poi','restricted_stock_deferred',
-                'from_messages','to_messages','deferral_payments','long_term_incentive','to_poi_frac','from_poi_frac',
-                'director_fees','restricted_stock']
 
-scaler = MinMaxScaler((0,1),copy=True)
-
-#Trying a linear SVM
 clf = svm.SVC(kernel = 'linear', C = 100000, max_iter = 20000)
-pipe1 = Pipeline([('scaler',scaler),('clf',clf)])
-dump_classifier_and_data(pipe1,my_dataset, all_features)
+dump_classifier_and_data(clf, my_dataset, features_list2)
 tester.main()
 
-#Trying a rbf SVM
-clf = svm.SVC(C = 100000, max_iter = 20000)
-pipe2 = Pipeline([('scaler',scaler),('clf',clf)])
-dump_classifier_and_data(pipe2,my_dataset, all_features)
-tester.main()
-
-#Trying a DecisionTree
+    #Trying a DecisionTree
 clf = DecisionTreeClassifier()
-pipe3 = Pipeline([('scaler',scaler),('clf',clf)])
-dump_classifier_and_data(pipe3,my_dataset, all_features)
+dump_classifier_and_data(clf, my_dataset, features_list2)
 tester.main()
 
-#Trying a Naive bayes
+    #Trying a Naive bayes
 clf = GaussianNB()
-dump_classifier_and_data(clf,my_dataset, all_features)
+dump_classifier_and_data(clf, my_dataset, features_list2)
 tester.main()
+
+
+'''Results for above sample tests'''
+
+#SVM Metrics
+print 'Linear SVM Metrics','C=100000,iter=20000',\
+    'Accuracy: 0.61327 Precision: 0.16780	Recall: 0.48000	F1: 0.24867	F2: 0.34983','\n'
+
+#Not a great scores but there is room for improvements
+
+#DecisionTrees
+print 'Decision Trees',\
+    'Accuracy: 0.83393	Precision: 0.37417	Recall: 0.36500	F1: 0.36953	F2: 0.36680','\n'
+
+#Accuracy, Precision, Recall, F1, F2 scores are good for Decision trees
+
+#Gaussian Naive Bayes
+print 'Gaussian NB metrics',\
+    'Accuracy: 0.84487	Precision: 0.37548	Recall: 0.24650	F1: 0.29762	F2: 0.26468','\n'
 
 #Accuracy, Precisions are good, but not a great recall.
 
-print 'Linear SVM, Gaussian NB did not perform well...\n'
-
-print 'Proceeding further with Gaussian SVM and Decision tree classifiers\n'
+print 'Currently all three models looks like a good candidate for further tuning\n'
 
 print '!!!!!!!!!!SAMPLE TRAINING COMPLETED!!!!!!!!!!','\n\n'
-
-pause()
-
-print '################# UNDER SAMPLING & OVER SAMPLING ###################'
-
-print '\nUndersampling the non-poi data points from 127 to 70\n'
-
-print 'Oversampling the POI data points from 18 to 30 by choosing three random ' \
-      'samples of 10\n'
-
-mod_dataset = balance_data(my_dataset)
-
-pause()
-
-print '###################### Feature Selection ######################'
 
 '''
 ### Task 5: Tune your classifier to achieve better than .3 precision and recall
@@ -210,114 +202,146 @@ print '###################### Feature Selection ######################'
 ### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
 '''
 
-print '\nRunning a battery of fit/test using the below feature scores from K-best..'
+#Further tuning to be done using feature_scaling, different model parameters
 
+#Creating the scaler object to scale some high valued features
 
-#Extracting all features
-all_feat = my_dataset['METTS MARK'].keys()
-all_feat.remove('email_address')
-all_feat.insert(0,all_feat.pop(all_feat.index('poi')))
+shufflesplit = StratifiedShuffleSplit(n_splits = 10, test_size = 0.1, random_state = 42)
+scaler = MinMaxScaler((0,1), copy=False)
 
-print '\nBelow are the scores for the features in descending order\n'
+'''Testing all the models below using feature_list2 which do not
+includes the numerical value of to_POI and from_POI emails'''
 
-kbest_scores(mod_dataset,all_feat)
-
-print '\n'
-
-pause()
-
-#Below function call will test for different combination
-
-print 'Multiple fit/tests run on combinations of featues, model, parameres using 100, 1000 folds\n'
-
-print 'Please set testall = TRUE at the top of the document to see the results of all the test..\n'
-
-print 'WARNING: Running all tests would take good amount of time...\n'
-
-pause()
+#testing a Decision Tree
 
 if testall:
-    feature_filter(mod_dataset,all_feat)
+    print 'Training/Testing a DecisionTree'
+    fit_multiple(my_dataset,features_list2,'DT',scaler)
+
+    '''Training & testing a Gaussian SVM classifier for variety of values
+    #After testing many C values&iterations, zeroed in on below values'''
+
+    print 'Training/Testing a Gaussian SVM\n'
+
+    cvalues = [100000,110000]
+    iterations = [15000,20000,90000]
+    fit_multiple(my_dataset, features_list2, 'SVM',scaler,cvalues = cvalues,
+                 iterations = iterations)
+
+    #training a Gaussian NB classifier
+    print 'Training/Testing a GaussianNB\n'
+
+    fit_multiple(my_dataset, features_list2, 'NB', scaler)
+
+    '''Training & testing below models using feature_list1 which includes both
+    numerical and fraction values of to_poi and from_poi'''
+
+    #Decision Tree train and test
+
+    fit_multiple(my_dataset,features_list1,'DT',scaler)
+
+    #Repeating the SVM classifier training with feature_list1
+
+    fit_multiple(my_dataset, features_list1, 'SVM',scaler,cvalues = cvalues,
+                iterations = iterations)
+
+    #Training a GaussianNB classifier
+
+    fit_multiple(my_dataset, features_list1, 'NB',scaler)
 
 
 '''Below are the results for different parameters for above tests after Tuning'''
 
 
-print '\n#################### METRICS ON MULTIPLE MODELS & FEATURES ####################\n'
+print '\n#################### METRICS ON MULTIPLE MODELS ####################\n'
 
-print 'Metrics for SVM & DT model with different features chosen by the grid search\n'
+print 'Metrics for SVM with feature_list2\n'
 
 #Few Metrics for Gaussian SVM with Feature_list2
 
-print 'Below features have been selected out of 8 features tested a 100 fold test...\n'
+'''
+C = 10000 & iter = [1000,5000,10000,15000,20000,100000] --> Fine Precision but not good enough recall
+C = 15000 & iter = [1000,5000,10000,15000,20000,100000] --> not good enough Precision and recall
+C = 20000 & iter = 1000 --> good enough recall(0.38), bad precision(0.28)
+C = 20000 & iter = [5000,10000,15000,20000,100000] --> Good enough precision (0.35 - 0.36) bad recall (0.27)
+'''
 
+print 'C = 10000 & iter = [1000,5000,10000,15000,20000,100000] --> ' \
+      'Fine Precision but not good enough recall'
+print 'C = 15000 & iter = [1000,5000,10000,15000,20000,100000] --> ' \
+      'not good enough Precision and recall'
+print 'C = 20000 & iter = 1000 --> good enough recall(0.38), ' \
+      'bad precision(0.28)'
+print 'C = 20000 & iter = [5000,10000,15000,20000,100000] --> ' \
+      'Good enough precision (0.35 - 0.36) bad recall (0.27)\n'
 
-print '### Feature - 1 ###\n'
-print ['poi', 'other', 'to_messages', 'bonus', 'from_this_person_to_poi', 'restricted_stock',
-       'from_poi_to_this_person','deferral_payments', 'loan_advances','salary', 'total_stock_value', 'total_payments'],'\n'
+#Few good metrics for Gaussian SVM with Feature_list2
+'''
+C = 100000 & iter = 5000  --> Good Precision: 0.33002 & Good Recall: 0.36550
+C = 100000 & iter = 20000  ---> Good Precision: 0.33202	& Good Recall: 0.33650
+C = 100000 & iter = 100000 --> Good Precision: 0.33534 & Good	Recall: 0.33500
+C = 110000 & iter = 110000 --> Good Precision: 0.37507 & Good Recall: 0.34000
+C = 100000 & iter = 110000 --> Good Precision: 0.38311 & Good	Recall: 0.34250, F1 0.36167, F2 0.34992
+'''
 
-print 'Gaussian SVM: ','C=100000,iter=10000,Tolerance = 0.5'
-print 'Precision: 0.715098094967 ', 'Recall: 0.838333333333\n'
-print 'Decision Tree:','Min_Split: 2'
-print 'Precision: 0.644107050952 ','Recall: 0.834333333333\n'
+print 'C = 100000 & iter = 5000  --> Good Precision: ' \
+      '0.33002 & Good Recall: 0.36550'
+print 'C = 100000 & iter = 20000  ---> ' \
+      'Good Precision: 0.33202 & Good Recall: 0.33650'
+print 'C = 100000 & iter = 100000 --> Good Precision: 0.33534 ' \
+    '& Good	Recall: 0.33500'
+print 'C = 110000 & iter = 110000 --> Good Precision: 0.37507 ' \
+    '& Good Recall: 0.34000'
+print 'C = 100000 & iter = 110000 --> Good Precision: 0.38311 ' \
+    '& Good	Recall: 0.34250, F1 0.36167, F2 0.34992\n'
 
-print '### Feature - 4 ###\n'
-print ['poi', 'bonus', 'exercised_stock_options', 'to_poi_frac', 'restricted_stock', 'loan_advances','total_payments',
-       'other', 'director_fees', 'deferral_payments'],'\n'
+#Few Metrics for Gaussian SVM with feature_list1
+print 'Metrics for SVM with feature_list1','\n'
+'''
+C 100000 #ofiterations 10000 -- Accuracy: 0.83687	Precision: 0.37886	Recall: 0.34950	F1: 0.36359	F2: 0.35500
+C 100000 #ofiterations 15000 -- Accuracy: 0.83773	Precision: 0.38155	Recall: 0.34950	F1: 0.36482	F2: 0.35547
+C 100000 #ofiterations 20000 -- Accuracy: 0.83853	Precision: 0.38355	Recall: 0.34750	F1: 0.36464	F2: 0.35416
+C 100000 #ofiterations 110000-- Accuracy: 0.83880	Precision: 0.38311	Recall: 0.34250	F1: 0.36167	F2: 0.34992
+'''
 
-print 'Gaussian SVM: ','C=100000,iter=15000,Tolerance = 0.01'
-print 'Precision: 0.744804655029 ', 'Recall: 0.896\n'
-print 'Decision Tree:','Min_Split: 2'
-print 'Precision: 0.725137150091 ','Recall: 0.859166666667\n'
+print 'C 100000 #ofiterations 10000 -- Accuracy: 0.83687	Precision: 0.37886	' \
+      'Recall: 0.34950	F1: 0.36359	F2: 0.35500'
+print 'C 100000 #ofiterations 15000 -- Accuracy: 0.83773	Precision: 0.38155	' \
+      'Recall: 0.34950	F1: 0.36482	F2: 0.35547'
+print 'C 100000 #ofiterations 20000 -- Accuracy: 0.83853	Precision: 0.38355	' \
+      'Recall: 0.34750	F1: 0.36464	F2: 0.35416'
+print 'C 100000 #ofiterations 110000-- Accuracy: 0.83880	Precision: 0.38311	' \
+      'Recall: 0.34250	F1: 0.36167	F2: 0.34992\n'
 
-print '### Feature - 6 ###\n'
-print ['poi', 'loan_advances', 'restricted_stock', 'long_term_incentive', 'restricted_stock_deferred','other', 'bonus',
-       'shared_receipt_with_poi', 'from_poi_frac', 'from_poi_to_this_person', 'salary','exercised_stock_options'],'\n'
+#Metrics for Decision Tree using feature_list2
+print 'Metrics for Decision Tree using feature_list2\n'
 
-print 'Gaussian SVM: ','C=60000,iter=3500,Tolerance = 0.7'
-print 'Precision: 0.737403928266 ', 'Recall: 0.8635\n'
-print 'Decision Tree:','Min_Split: 2'
-print 'Precision: 0.658562367865 ','Recall: 0.830666666667\n'
+'''
+Accuracy: 0.83380	Precision: 0.37060	Recall: 0.35300	F1: 0.36159	F2: 0.35639
+'''
 
-print '### Feature - 7 ###\n'
-print ['poi', 'shared_receipt_with_poi', 'director_fees', 'other', 'deferral_payments', 'to_poi_frac','to_messages',
-       'total_payments', 'from_poi_to_this_person', 'expenses', 'salary', 'long_term_incentive'],'\n'
+print 'Accuracy: 0.83380	Precision: 0.37060	Recall: 0.35300	F1: 0.36159	F2: 0.35639\n'
 
-print 'Gaussian SVM: ','C=100000,iter=5000,Tolerance = 0.5'
-print 'Precision: 0.6751695665 ', 'Recall: 0.763166666667\n'
-print 'Decision Tree:','Min_Split: 2'
-print 'Precision: 0.72567114094 ','Recall: 0.865\n'
+#Metrics for Decision Tree using feature_list2
+print 'Metrics for Decision Tree using feature_list1\n'
+'''
+Accuracy: 0.83233	Precision: 0.36609	Recall: 0.35200	F1: 0.35891	F2: 0.35473
+'''
 
-pause()
+print 'Accuracy: 0.83233	Precision: 0.36609	Recall: 0.35200	F1: 0.35891	F2: 0.35473\n'
 
-print 'Based on the above metrics, a final feature list was selected..\n'
-
-print ['poi','other','bonus','salary','restricted_stock','loan_advances','total_payments','to_poi_frac',
-       'deferral_payments','from_poi_frac','shared_receipt_with_poi','long_term_incentive'],'\n'
-
-
-print 'This final feature was fit/tested on SVM & DT for various parametres..\n'
-
-print 'Gaussian SVM: ','C=60000,iter=3500,Tolerance = 0.7'
-print 'Precision: 0.733089021225 ', 'Recall: 0.800166666667\n'
-print 'Decision Tree:','Min_Split: 4'
-print 'Precision: 0.699665831245 ','Recall: 0.8375\n'
-
+#Gaussian NBs did not have a good recall values
 
 '''After analyzing different models and associated parameters, choosing the below
 model and parameters,Below model and parameters gave the best of all Precision,
 Recall, F1,F2 scores'''
 
-pause()
 
 print '#################### CHOSEN MODEL & PARAMS #####################\n'
-print 'Dataset:','Modified Dataset'
 print 'Model:','SVM'
 print 'Kernel:','Gaussian'
-print 'C:60000 , Iterations:3500'
-print 'Feature : ',['poi','other','bonus','salary','restricted_stock','loan_advances','total_payments','to_poi_frac',
-       'deferral_payments','from_poi_frac','shared_receipt_with_poi','long_term_incentive']
-
+print 'C:100000 , Iterations:20000'
+print 'Feature : feature_list1'
 
 # Example starting point. Try investigating other evaluation techniques!
 # from sklearn.cross_validation import train_test_split
@@ -331,13 +355,9 @@ print 'Feature : ',['poi','other','bonus','salary','restricted_stock','loan_adva
 
 print '\n################### FINAL RESULTS ######################\n'
 
-
-final_feature = ['poi','other','bonus','salary','restricted_stock','loan_advances','total_payments','to_poi_frac',
-       'deferral_payments','from_poi_frac','shared_receipt_with_poi','long_term_incentive']
-
-svm_clf = svm.SVC(kernel = 'rbf',C = 60000, max_iter = 3500)
+svm_clf = svm.SVC(kernel = 'rbf',C = 100000, max_iter = 20000)
 ppl = Pipeline([('scaler',scaler),('classifier',svm_clf)])
-dump_classifier_and_data(ppl, mod_dataset, final_feature)
+dump_classifier_and_data(ppl, my_dataset, features_list1)
 tester.main()
 
 #Addition models based on emails
